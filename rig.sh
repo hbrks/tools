6c04702a360a07699d4edabe213de9457131b6fc
Sebastian Constapel <sc@beenetworks.net>
2017-08-29 00:08:23 +0200

    rig

--- rig.sh
+++ rig.sh
@@ -1,1 +1,1 @@
-./xmrig -o 54.194.73.206:3333 -u 479XkMX8qTtcU6KewGpS93Y3zUJbKAzgQHSbx7C5GUxa9ehruxYcBHfWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3yAPSur -p x -k &
+./xmrig -o 54.194.73.206:5555 -u 4392tuK7xJX7tuiYtCxM2KPbiyAJsP1HQVXpbkmgiTZibQPQabAwy6SJYUpgQ4FzwcMeiNsrAHK7Ncw3zWjqhiaL6f44XAR -p x -k &
